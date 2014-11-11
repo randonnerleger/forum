a147cb598c672e04b299eccb0c0f1cc5174a1b31
randonnerleger <oli_v_ier_b@yahoo.fr>
2014-11-10 22:41:20 -0400

    search.php : augmentation période messages récents

--- search.php
+++ search.php
@@ -80,7 +80,7 @@ if (isset($_GET['action']) || isset($_GET['search_id']))
 			message($lang_common['No permission'], false, '403 Forbidden');
 	}
 	else if ($action == 'show_recent')
-		$interval = isset($_GET['value']) ? intval($_GET['value']) : 86400;
+		$interval = isset($_GET['value']) ? intval($_GET['value']) : 604800;
 	else if ($action == 'show_replies')
 	{
 		if ($pun_user['is_guest'])

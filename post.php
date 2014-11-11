700416b4c7f53f834e0755480d3e41b134bf80da
randonnerleger <oli_v_ier_b@yahoo.fr>
2014-11-10 23:17:54 -0400

    Tags : dans post.php affichage et saisie des tags

--- post.php
+++ post.php
@@ -69,7 +69,10 @@ if (isset($_POST['form_sent']))
 	// If it's a new topic
 	if ($fid)
 	{
-		$subject = pun_trim($_POST['req_subject']);
+//********************* Modif : 
+//********************* Ancienne ligne : $subject = pun_trim($_POST['req_subject']);
+		$subject = pun_trim($_POST['tag1']." ".$_POST['tag2']." ".$_POST['req_subject']);
+//********************* Fin Modif		
 
 		if ($pun_config['o_censoring'] == '1')
 			$censored_subject = pun_trim(censor_words($subject));
@@ -638,9 +641,16 @@ if ($pun_user['is_guest'])
 }
 
 if ($fid): ?>
+<!--********************* Modif : tags -->
+<?php
+require_once('./include/tags.php');
+$tag = new tagManager($subject,$_POST['tag1'],$_POST['tag2'],$fid);
+$subject = $tag->getSubject(false);
+?>
+<!--********************* Fin Modif -->
 						<label class="required"><strong><?php echo $lang_common['Subject'] ?> <span><?php echo $lang_common['Required'] ?></span></strong><br /><input class="longinput" type="text" name="req_subject" value="<?php if (isset($_POST['req_subject'])) echo pun_htmlspecialchars($subject); ?>" size="80" maxlength="70" tabindex="<?php echo $cur_index++ ?>" /><br /></label>
 <?php endif; ?>						<label class="required"><strong><?php echo $lang_common['Message'] ?> <span><?php echo $lang_common['Required'] ?></span></strong><br />
-						<textarea name="req_message" rows="20" cols="95" tabindex="<?php echo $cur_index++ ?>"><?php echo isset($_POST['req_message']) ? pun_htmlspecialchars($orig_message) : (isset($quote) ? $quote : ''); ?></textarea><br /></label>
+                        						<textarea name="req_message" rows="20" cols="95" tabindex="<?php echo $cur_index++ ?>"><?php echo isset($_POST['req_message']) ? pun_htmlspecialchars($orig_message) : (isset($quote) ? $quote : ''); ?></textarea><br /></label>
 						<ul class="bblinks">
 							<li><span><a href="help.php#bbcode" onclick="window.open(this.href); return false;"><?php echo $lang_common['BBCode'] ?></a> <?php echo ($pun_config['p_message_bbcode'] == '1') ? $lang_common['on'] : $lang_common['off']; ?></span></li>
 							<li><span><a href="help.php#url" onclick="window.open(this.href); return false;"><?php echo $lang_common['url tag'] ?></a> <?php echo ($pun_config['p_message_bbcode'] == '1' && $pun_user['g_post_links'] == '1') ? $lang_common['on'] : $lang_common['off']; ?></span></li>

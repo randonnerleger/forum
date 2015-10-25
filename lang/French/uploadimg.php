91c9f3fc4f0b59375e8d8811220a2b052d00ac32
oli_v_ier <oli_v_ier@yahoo.fr>
2015-10-25 04:33:00 -0400

    Plugin Uploadimg : 5 fichiers à la racine + 1 fichier langue

---
+++ lang/French/uploadimg.php
@@ -0,0 +1,72 @@
+<?php
+
+// Language definitions used by the Automatic Image Upload with Thumbnails-mod
+// French language by Super_baloo8
+$lang_uploadimg = array(
+
+//Upload main
+'Upload image'          =>  'Envoyer une image',  // title of page
+'Uploads'               =>  'Envoi',
+'Select image'          =>  'Choisir l\'image à envoyer',
+'Upload message'        =>  'Utilisez ce formulaire pour envoyer une image vers votre album. Une miniature sera automatiquement créée.<br><strong>Note</strong>: l\'hébergement d\'images sur ce site doit avant tout être une contribution au site. Tout abus de ce service sera sanctionné par une fermeture du compte et la suppression de toutes les images.',
+'Restrictions'          =>  'Restrictions :',
+'Size limit'            =>  'Le fichier doit faire moins de <MAX_SIZE>Ko',
+'link to resize'        =>  ' (<a href="http://webresizer.com/resizer/" target="blank">un outil pour alléger vos images</a>)',
+'Images above'          =>  'Les images plus lourdes que <MAX_SIZE>Ko seront automatiquement redimensionnées en 800x600 pixels.<br /> Si votre image est très lourde ou que le débit de votre connexion est faible, l\'attente peut être longue: <a href="http://webresizer.com/resizer/" target="blank">voici un outil pour alléger vos images</a>.',
+'Resize image'          =>  'redimensionner l\'image ?',
+'Avatar'                =>  'avatar',
+'Thumbnail'             =>  'miniature',
+'Websites and email'    =>  'pour les sites Internet',
+'Message boards'        =>  'pour le forum',
+'File'                  =>  'Fichier :',
+'Result'                =>  'Résultat',
+'Successful upload'     =>  'L\'image a été envoyée avec succès.',
+'Successful thumbnail'  =>  'La miniature a été créée avec succès.',
+'My uploads'            =>  '<a href="/forum/uploadimg_view.php?view=gallery">En diaporama</a> | <a href="/forum/uploadimg_view.php?view=list">En liste</a>',
+
+//Messages
+'No permission'         =>  'Vous n\'avez pas l\'autorisation pour envoyer des images. Contactez l\'administrateur du forum pour plus d\'informations.',
+'Upload disabled'       =>  'Vous ne pouvez pas envoyer d\'images - fonction désactivée',
+'No size limit'         =>  'Aucune restriction de taille',
+'Unable to upload'      =>  'ERREUR : Impossible de charger l\'image.',
+'Wrong filetype'        =>  'ERREUR : Mauvais type de fichier',
+'Can only upload1'      =>  'Vous ne pouvez envoyer que les fichiers <type1>',
+'Can only upload2'      =>  'Vous ne pouvez envoyer que les fichiers <type1> et <type2>',
+'Can only upload3'      =>  'Vous ne pouvez envoyer que les fichiers <type1>, <type2> et <type3>',
+'Has to be1'            =>  '(doit être un .<extension1>)',
+'Has to be2'            =>  '(doit être un .<extension1> ou .<extension2>)',
+'Has to be3'            =>  '(doit être un .<extension1>, .<extension2> ou .<extension3>)',
+'Yours is'              =>  'Le votre est :',
+'File exists'           =>  'Le fichier existe déjà - Vous ne pouvez pas remplacer un fichier existant',
+'Exist message'         =>  'Le fichier que vous essayez d\'envoyer :',
+'Too big'               =>  'Le fichier est trop gros. Le fichier doit faire moins de <MAX_SIZE>Ko.',
+
+//Statistics table
+'Upload statistics'     =>  'Statistiques des envois',
+'Userid'                =>  'ID Utilisateur',
+'Megabyte'			   	=>	'Mo',
+'Kilobyte'			   	=>	'Ko',
+'Totals'                =>  'Totals :',
+'Uploaders'             =>  'envoyeur',
+'Deleted user'          =>  'Utilisateur supprimé',
+
+//View uploads table
+'Filename'              =>  'Nom',
+'Size'                  =>  'Taille',
+'Filetype'              =>  'Type',
+'Dimensions'		    =>	'Dimensions',
+'Date'                  =>  'Date de modification',
+'Get code'              =>  'BBCode',  // link displaying the BBCode
+'Image'                 =>  'Image&nbsp;',       // PNG Image, JPG Image etc
+'Images'			    =>	'images',
+'Thumb' 			    =>	'Miniature&nbsp;',
+'Unknown filetype'      =>  'Type inconnu',
+'View as list'          =>  'Voir en liste',
+'View as gallery'       =>  'Voir en gallerie',
+'Copy and paste'        =>  'Copier et coller le texte dans votre message ',
+'Delete cap'            =>  'Supprimer',
+'Delete'                =>  'supprimer',
+'Delete confirm'        =>  'Etes vous sûr de vouloir supprimer ?',
+'Back'                  =>  'Retour'
+
+);
---
+++ uploadimg_config.php
@@ -0,0 +1,128 @@
+<?php
+
+  /*****************************************************
+  ** Script configuration
+  *****************************************************/
+
+$idir = "uploads/";   // Path To Images Directory
+$tdir = "uploads/thumbs/";   // Path To Thumbnails Directory
+
+$twidth = "200";   // Maximum Width For Thumbnail Images - default = 150
+$theight = "200";   // Maximum Height For Thumbnail Images - default = 150
+$size_limit = "no"; // **** if "yes" there will be a file size limit for uploaded files
+$limit_size = "200"; // file size limit in KB - default = 200
+
+$allow_resize_option = "1"; // allow = 1 ; disallow = 0
+
+$resize_images_above_limit = "yes"; // if "yes" images exceeding 'limit_size' will be automatically resized to 640x480
+                                    // - must set $size_limit = "no" for this to work
+
+$allow_jpg_uploads = "1"; // allow = 1 ; disallow = 0
+$allow_png_uploads = "1"; // allow = 1 ; disallow = 0
+$allow_gif_uploads = "1"; // allow = 1 ; disallow = 0
+
+// ** Gallery settings
+$images_per_page = "20"; // number of images displayed per page in the gallery
+$images_per_row = "4"; 
+$display_img_box = "1"; // allow = 1 ; disallow = 0
+
+
+  /*****************************************************
+  ** Permission setting
+  *****************************************************/
+
+$Allow_Uploads = array();
+//
+// Listed below are user groups that are are allowed to upload images.
+//
+// $Allow_Uploads[] = "MY_USERGROUP";
+//
+$Allow_Uploads[] = "Administrateurs";
+$Allow_Uploads[] = "Moderateurs";
+$Allow_Uploads[] = "Membres";
+$Allow_Uploads[] = "Doit se calmer";
+$Allow_Uploads[] = "En lecture seule";
+$Allow_Uploads[] = "Exclu forum Achat-Vente-Don-Troc";
+$Allow_Uploads[] = "En discussion avec les modos MAIS en lecture seule";
+$Allow_Uploads[] = "En discussion avec les modos ET membre";
+$Allow_Uploads[] = "Membre des projets de l'association";
+$Allow_Uploads[] = "Membre des projets du site";
+$Allow_Uploads[] = "Membre des projets site + association";
+$Allow_Uploads[] = "Membre du comite directeur ou invite";
+$Allow_Uploads[] = "Membre du bureau de l'association";
+$Allow_Uploads[] = "Groupe rien que pour Nutzzz";
+$Allow_Uploads[] = "PRO";
+$Allow_Uploads[] = "Developpeurs";
+$Allow_Uploads[] = "Gestion du bestof";
+$Allow_Uploads[] = "Groupe pour Opitux";
+$Allow_Uploads[] = "Groupe concours photo";
+$Allow_Uploads[] = "Groupe pour Dolgan";
+$Allow_Uploads[] = "Groupe pour CSC";
+$Allow_Uploads[] = "Groupe pour Rhafiki";
+$Allow_Uploads[] = "Groupe pour Ralf7500113";
+
+/*
+The permission setting allows you to have full control over who can upload images.
+
+If you want all members to be able to upload images, just add the following line:
+$Allow_Uploads[] = "Members";
+
+To ban a certain user from uploading images, just move the user to an user group not
+included in the  $Allow_Uploads array.
+
+For those who do not know how to create new user groups:
+Log in as administrator and go to Administration->User groups->Add new group
+*/
+
+
+  /*****************************************************
+  ** Statistics setting
+  *****************************************************/
+
+$Allow_Stats = array();
+//
+// Listed below are user groups that are are allowed to view upload statistics.
+//
+// $Allow_Stats[] = "MY_USERGROUP";
+//
+$Allow_Stats[] = "Administrateurs";
+$Allow_Stats[] = "Moderateurs";
+//$Allow_Stats[] = "Uploaders";
+//$Allow_Stats[] = "Membres";
+
+
+  /*****************************************************
+  ** Delete setting
+  *****************************************************/
+
+$Allow_Delete = array();
+//
+// Listed below are user groups that are allowed to delete their own uploaded images.
+// Note: Only Administrators can delete images of another user
+//
+// $Allow_Delete[] = "MY_USERGROUP";
+//
+
+$Allow_Delete[] = "Administrateurs";
+$Allow_Delete[] = "Moderateurs";
+$Allow_Delete[] = "Membres";
+$Allow_Delete[] = "Doit se calmer";
+$Allow_Delete[] = "Exclu forum Achat-Vente-Don-Troc";
+$Allow_Delete[] = "En discussion avec les modos ET membre";
+$Allow_Delete[] = "Membre des projets de l'association";
+$Allow_Delete[] = "Membre des projets du site";
+$Allow_Delete[] = "Membre des projets site + association";
+$Allow_Delete[] = "Membre du comite directeur ou invite";
+$Allow_Delete[] = "Membre du bureau de l'association";
+$Allow_Delete[] = "Groupe rien que pour Nutzzz";
+$Allow_Delete[] = "PRO";
+$Allow_Delete[] = "Developpeurs";
+$Allow_Delete[] = "Gestion du bestof";
+$Allow_Delete[] = "Groupe pour Opitux";
+$Allow_Delete[] = "Groupe concours photo";
+$Allow_Delete[] = "Groupe pour Dolgan";
+$Allow_Delete[] = "Groupe pour CSC";
+$Allow_Delete[] = "Groupe pour Rhafiki";
+$Allow_Delete[] = "Groupe pour Ralf7500113";
+
+?>

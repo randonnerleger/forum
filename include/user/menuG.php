8b95520d181071c605d5487ab8c564e7b29624d8
Frederic Caffin <fc@xymail.fr>
2019-02-08 10:31:58 +0100

    Connexion à partir du wiki et redirection vers le wiki

--- include/user/menuG.php
+++ include/user/menuG.php
@@ -11,6 +11,10 @@
 			$forum_nouveaux_messages ='';
 			$forum_sans_reponses ='';
 			$forum_profil ='';
+			// Opitux
+			// Connexion à partir du wiki
+			$redirect_url = $site_url . htmlentities($_SERVER['REQUEST_URI']);
+			$redirect_url = str_replace('/wiki','/forum/wiki', $redirect_url)
 		?>
 						<li class="display"><strong>Identification</strong>
 							<input id="menug-identification" type="checkbox" name="toggle" />
@@ -18,7 +22,7 @@
 							<div class="content-togglebox">
 								<form id="login_menuG" method="post" action="<?php echo path_to_forum . 'login.php?action=in'; ?>" onsubmit="return process_form(this)">
 									<input type="hidden" name="form_sent" value="1" />
-									<input type="hidden" name="redirect_url" value="<?php echo $site_url . htmlentities($_SERVER['REQUEST_URI']); ?>" />
+									<input type="hidden" name="redirect_url" value="<?php echo $redirect_url; ?>" />
 									<input type="hidden" name="csrf_token" value="<?php echo pun_csrf_token() ?>" />
 
 									<input placeholder="Nom d'utilisateur" class="fastlogin" id="username" type="text" name="req_username" size="10" />

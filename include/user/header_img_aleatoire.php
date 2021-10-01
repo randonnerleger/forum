751de9b147fad99f7b96c492f5eb4ac3fc326c55
opitux <f.caffin@xymail.fr>
2021-10-01 09:18:36 +0200

    Fix blur background quand theme clair ou sombre specifique
    L'image générique était appelée en blur background au lieu de l'image 
    specifique

--- include/user/header_img_aleatoire.php
+++ include/user/header_img_aleatoire.php
@@ -1,4 +1,22 @@
 <?php
+global $pun_user;
+
 $numheader = ( isset($_GET['numheader']) && (int) $_GET["numheader"] > 0 ) ? sprintf("%02d", (integer) $_GET['numheader'] ) : strftime("%U");
+
+$style = array(
+	'alone',
+	'ciel_et_terre',
+	'cuben',
+	'eclaircie',
+	'herbage',
+	'islande',
+	'montagne',
+	'old_school',
+	'un_lac',
+	'zen'
+);
+$pun_user_style = str_replace( array ('RL_Clair_', 'RL_Sombre_' ), '', $pun_user['style'] );
 ?>
-<style>#header, #header .blur::before {background-image: url("<?php echo path_to_rl; ?>tpl/img/header_<?php echo $numheader; ?>.jpg");}</style>
+<style>#header, #header .blur::before {
+	background-image: url("<?php echo path_to_rl; ?>tpl/img/header_<?php echo strtolower( in_array( $pun_user_style, $style ) ? $pun_user_style : $numheader ); ?>.jpg");}
+</style>

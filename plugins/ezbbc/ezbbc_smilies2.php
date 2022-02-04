ac4aba269557a61d4ddf63e2dac4268a447edff4
opitux <f.caffin@xymail.fr>
2022-02-04 15:48:45 +0100

    fix: dimension des nouveaux smileys

--- plugins/ezbbc/ezbbc_smilies2.php
+++ plugins/ezbbc/ezbbc_smilies2.php
@@ -4,6 +4,9 @@ require PUN_ROOT.'plugins/ezbbc/config.php';
 if ($ezbbc_config['smilies_set'] == 'ezbbc_smilies'):
 $text = preg_replace("#(?<=[>\s])".preg_quote($smiley_text, '#')."(?=\W)#m", '<img src="'.pun_htmlspecialchars((function_exists('get_base_url') ? get_base_url(true) : $pun_config['o_base_url']).'/plugins/ezbbc/style/smilies/'.$smiley_img).'" alt="'.substr($smiley_img, 0, strrpos($smiley_img, '.')).'" />', $text);
 else:
-$text = preg_replace("#(?<=[>\s])".preg_quote($smiley_text, '#')."(?=\W)#m", '<img src="'.pun_htmlspecialchars((function_exists('get_base_url') ? get_base_url(true) : $pun_config['o_base_url']).'/img/smilies/'.$smiley_img).'" width="15" height="15" alt="'.substr($smiley_img, 0, strrpos($smiley_img, '.')).'" />', $text);
+// $text = preg_replace("#(?<=[>\s])".preg_quote($smiley_text, '#')."(?=\W)#m", '<img src="'.pun_htmlspecialchars((function_exists('get_base_url') ? get_base_url(true) : $pun_config['o_base_url']).'/img/smilies/'.$smiley_img).'" width="15" height="15" alt="'.substr($smiley_img, 0, strrpos($smiley_img, '.')).'" />', $text);
+// MODIF OPITUX
+$text = preg_replace("#(?<=[>\s])".preg_quote($smiley_text, '#')."(?=\W)#m", '<img class="smiley" src="'.pun_htmlspecialchars((function_exists('get_base_url') ? get_base_url(true) : $pun_config['o_base_url']).'/img/smilies/'.$smiley_img).'" width="15" height="15" alt="'.substr($smiley_img, 0, strrpos($smiley_img, '.')).'" />', $text);
+// END
 endif;
 ?>
